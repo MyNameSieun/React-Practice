e12ace2ac378b4c9734fa3a0e1eacf9078c1e94f
박시은 <charm516@naver.com>
2024-01-24 19:17:25 +0900

    feat: isDone 토글 기능 구현

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -2,27 +2,34 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [title, setTitle] = useState();
-  const [content, setContent] = useState();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [todos, setTodos] = useState([]);
 
   const handlerAddBtn = (e) => {
     e.preventDefault();
     const newTodos = {
       id: uuidv4(),
-      title: "제목3",
-      content: "내용3",
+      title,
+      content,
       isDone: false,
     };
+    console.log(title);
+    if (title === "") {
+      alert("제목을 입력하세요.");
+      return;
+    }
+    if (content === "") {
+      alert("내용을 입력하세요.");
+      return;
+    }
+
     setTodos([...todos, newTodos]);
-  };
-  const handlerDelateBtn = (id) => {
-    const newTodos = todos.filter(function (item) {
-      return item.id !== id;
-    });
-    setTodos(newTodos);
+    setTitle("");
+    setContent("");
   };
 
   return (
@@ -30,12 +37,14 @@ function App() {
       <form>
         제목 :{" "}
         <input
+          value={title}
           onChange={function (e) {
             setTitle(e.target.value);
           }}
         />
         내용 :{" "}
         <input
+          value={content}
           onChange={function (e) {
             setContent(e.target.value);
           }}
@@ -44,72 +53,8 @@ function App() {
           추가
         </button>
       </form>
-      <h2>할 일 목록 📑</h2>
-      <div>
-        {todos
-          .filter(function (todo) {
-            return todo.isDone === false;
-          })
-          .map(function (todo) {
-            return (
-              <div className="todo-item" key={todo.id}>
-                <div>{todo.title}</div>
-                <div>{todo.content}</div>
-                <button
-                  onClick={function () {
-                    const newTodos = todos.map(function (item) {
-                      if (todo.id === item.id) {
-                        return {
-                          ...item,
-                          isDone: !item.isDone,
-                        };
-                      } else {
-                        return item;
-                      }
-                    });
-                    setTodos(newTodos);
-                  }}
-                >
-                  완료
-                </button>
-                <button onClick={() => handlerDelateBtn(todo.id)}>삭제</button>
-              </div>
-            );
-          })}
-      </div>
-      <h2>완료🎇</h2>
-      <div>
-        {todos
-          .filter(function (todo) {
-            return todo.isDone === true;
-          })
-          .map(function (todo) {
-            return (
-              <div className="todo-item" key={todo.id}>
-                <div>{todo.title}</div>
-                <div>{todo.content}</div>
-                <button
-                  onClick={function () {
-                    const newTodos = todos.map(function (item) {
-                      if (todo.id === item.id) {
-                        return {
-                          ...item,
-                          isDone: !item.isDone,
-                        };
-                      } else {
-                        return item;
-                      }
-                    });
-                    setTodos(newTodos);
-                  }}
-                >
-                  완료취소
-                </button>
-                <button onClick={() => handlerDelateBtn(todo.id)}>삭제</button>
-              </div>
-            );
-          })}
-      </div>
+      <TodoList todos={todos} setTodos={setTodos} listIsDone={false} />
+      <TodoList todos={todos} setTodos={setTodos} listIsDone={true} />
     </main>
   );
 }
---
+++ todolist/src/components/TodoList.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function TodoList({ todos, setTodos, listIsDone }) {
+  const handlerDelateBtn = (id) => {
+    const newTodos = todos.filter(function (item) {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+  };
+  return (
+    <div>
+      {" "}
+      <h2>{listIsDone ? "완료 목록🤩" : "할 일 목록😭"}</h2>
+      <div>
+        {todos
+          .filter(function (todo) {
+            return todo.isDone === listIsDone;
+          })
+          .map(function (todo) {
+            return (
+              <div className="todo-item" key={todo.id}>
+                <div>{todo.title}</div>
+                <div>{todo.content}</div>
+                <button
+                  onClick={function () {
+                    const newTodos = todos.map(function (item) {
+                      if (todo.id === item.id) {
+                        return {
+                          ...item,
+                          isDone: !item.isDone,
+                        };
+                      } else {
+                        return item;
+                      }
+                    });
+                    setTodos(newTodos);
+                  }}
+                >
+                  완료
+                </button>
+                <button onClick={() => handlerDelateBtn(todo.id)}>삭제</button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
