ae0940889d47652dacffc153fac4e134ff36c03d
sieun <charm516@naver.com>
2024-09-15 15:44:33 +0900

    [feat] - todos.js 각 함수 반환 값 변경

--- 11_react_query/src/api/todos.js
+++ 11_react_query/src/api/todos.js
@@ -8,24 +8,29 @@ const todosAxios = axios.create({
 // todos 조회
 export const fetchTodos = async () => {
   const response = await todosAxios.get('/todos');
-  return response.data; // 데이터를 반환
+  return response.data;
 };
+
 // todos 작성
 export const addTodos = async (data) => {
-  return await todosAxios.post(`/todos`, data);
+  const response = await todosAxios.post('/todos', data);
+  return response.data;
 };
 
 // todos 삭제
 export const deleteTodos = async (id) => {
-  return await todosAxios.delete(`/todos/${id}`);
+  const response = await todosAxios.delete(`/todos/${id}`);
+  return response.data;
 };
 
 // todos 수정
 export const editTodos = async (id, data) => {
-  return await todosAxios.patch(`/todos/${id}`, data);
+  const response = await todosAxios.patch(`/todos/${id}`, data);
+  return response.data;
 };
 
 // todos toggle
 export const toggleTodos = async (id, isDone) => {
-  return await todosAxios.patch(`/todos/${id}`, { isDone: !isDone });
+  const response = await todosAxios.patch(`/todos/${id}`, { isDone: !isDone });
+  return response.data;
 };
